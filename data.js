08327389203a006e7d047f0cc5e447ff17e57172
alex-brad <alex.brad@live.co.uk>
2020-06-27 14:47:27 +0100

    tamarisk

--- data.js
+++ data.js
@@ -1,8 +1,110 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-kitchen",
-      "name": "kitchen",
+      "id": "0-back-porch",
+      "name": "Back Porch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3500,
+      "initialViewParameters": {
+        "yaw": 0.2519103453176861,
+        "pitch": -0.04756376041098065,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5689613600650478,
+          "pitch": 0.08013364508728671,
+          "rotation": 0,
+          "target": "1-hall"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hall",
+      "name": "Hall",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -3.093037655107242,
+        "pitch": 0.020818174840172787,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1003900240501956,
+          "pitch": -0.28367119040357025,
+          "rotation": 0,
+          "target": "4-bedroom-one"
+        },
+        {
+          "yaw": -1.6689570249779013,
+          "pitch": 0.2886640526620816,
+          "rotation": 0,
+          "target": "3-reception-room"
+        },
+        {
+          "yaw": 1.106487965457415,
+          "pitch": 0.15735269053352852,
+          "rotation": 0,
+          "target": "2-kitchen"
+        },
+        {
+          "yaw": -0.5085864129102546,
+          "pitch": 0.2602331451846034,
+          "rotation": 0,
+          "target": "0-back-porch"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-kitchen",
+      "name": "Kitchen",
       "levels": [
         {
           "tileSize": 256,
@@ -37,14 +139,14 @@ var APP_DATA = {
           "yaw": -0.6477194694751027,
           "pitch": 0.19171693880910823,
           "rotation": 0,
-          "target": "1-backporch"
+          "target": "1-hall"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-backporch",
-      "name": "backporch",
+      "id": "3-reception-room",
+      "name": "Reception Room",
       "levels": [
         {
           "tileSize": 256,
@@ -68,28 +170,70 @@ var APP_DATA = {
           "size": 4096
         }
       ],
-      "faceSize": 3500,
+      "faceSize": 4096,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 2.106520516240959,
+        "pitch": 0.059754345819960974,
+        "fov": 1.2634123859167636
       },
-      "linkHotspots": [],
-      "infoHotspots": [
+      "linkHotspots": [
         {
-          "yaw": 0.8790635831950357,
-          "pitch": -0.017710616650163047,
-          "title": "Title",
-          "text": "Text"
+          "yaw": 0.005879702806829812,
+          "pitch": 0.003272380331921454,
+          "rotation": 0,
+          "target": "1-hall"
         }
-      ]
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bedroom-one",
+      "name": "Bedroom One",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.07703043617152261,
+        "pitch": -0.0029727350256862906,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.473111446050307,
+          "pitch": 0.12494106350758116,
+          "rotation": 0,
+          "target": "1-hall"
+        }
+      ],
+      "infoHotspots": []
     }
   ],
   "name": "Tamarisk",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
     "viewControlButtons": true
   }
 };
